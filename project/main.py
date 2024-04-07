3e1f6c2e2536301a85d216dc5080f1cd2da52f54
Sansrita <sansritasaha@gmail.com>
2024-04-07 14:45:17 +0530

    scrapping TimesJob website

--- project/main.py
+++ project/main.py
@@ -1,18 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+
+print("Put some skills you are not familiar with....")
+unfamiliar_skill = input('>')
+print(f'Flitering Out {unfamiliar_skill}')
+
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
 #print(html_text)
 
-soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
-skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    for job in jobs:
+    
+        published_date = job.find('span', class_ = 'sim-posted').text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                  
+        #Prettifying the job posts
+        
+                print(f"Company Name: {company_name.strip()}")
+                print(f"Required Skills: {skills.strip()}")
+                print(f"More Info: {more_info}")
+    
 
+    #print(published_date)
 #print(skills)
 #print(company_name)
 
-print(f''' 
-Company Name: {company_name}
-Required Skills: {skills}
-''')
+#Without Prettifying the job posts
+        # print(f''' 
+        # Company Name: {company_name}
+        # Required Skills: {skills}
+        # ''')
+    
+                print('')
+                
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes.... ')
+        time.sleep(time_wait % 60)
