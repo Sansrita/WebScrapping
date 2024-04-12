b87be91bf2de28e15e5a1fdc34a5ef8be94f2447
Sansrita <sansritasaha@gmail.com>
2024-04-12 22:50:52 +0530

    project2_start

---
+++ Amazon_Scrapping/main.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+def get_title(soup):
+    
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+        
+        # Inner NavigatableString Object
+        title_value = title.text
+
+        # Title as a string value
+        title_string = title_value.strip()
+
+    except AttributeError:
+        title_string = ""
+
+    return title_string
